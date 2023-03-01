679debc247428f8984e460d2245811b6b3a99e0d
ginegatt <ilyashev4uck@yandex.by>
2023-03-02 02:24:17 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = FirstNumber;
+int SecondNumber;
+int ThirdNumber;
+if (SecondNumber > max){
+    SecondNumber = max;
+}
+Console.WriteLine($"max = {max}");
+
+// я не смог справиться с ошибкой cs0136 и cs0128
