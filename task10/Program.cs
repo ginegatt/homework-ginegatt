884e047a3de07e2c94b70a56ba13a4fd093ce627
ginegatt <ilyashev4uck@yandex.by>
2023-03-06 02:41:54 +0300

    task 10

---
+++ task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number < 100)
+{
+    Console.WriteLine("Неверное число");
+    return;
+}
+if (Number > 999)
+{
+    Console.WriteLine("Неверное число");
+    return;
+}
+
+int secondNumber = Number / 10 % 10;
+
+Console.WriteLine($"Второе число -> {secondNumber}");
