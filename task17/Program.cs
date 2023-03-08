7e6426f2f561eacc8a67d857dc4721a17a63685d
ginegatt <ilyashev4uck@yandex.by>
2023-03-09 01:08:25 +0300

    task 17

---
+++ task17/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine($"Введите координаты точки");
+Console.Write("X: ");
+int xCoord = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yCoord = Convert.ToInt32(Console.ReadLine());
