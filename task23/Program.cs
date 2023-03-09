d47f22ec71909546c4af9b667d92e83fdf2e21ea
ginegatt <ilyashev4uck@yandex.by>
2023-03-09 03:39:44 +0300

    task 21

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine($"Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void TableCube(int num)
+{
+    int index = 1;
+    int result = 0;
+    while (index <= num)
+    {
+        result = index * index * index;
+        Console.WriteLine($"{index}) {result}");
+        index++;
+    }
+}
+TableCube(number);
