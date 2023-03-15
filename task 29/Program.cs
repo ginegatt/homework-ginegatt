ddeffc7e7917397c8874f410b6b19aca7c74c182
ginegatt <ilyashev4uck@yandex.by>
2023-03-16 00:13:27 +0300

    task 25

---
+++ task 29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int lenArray = ReadInt("Введите длинну массива: ");
+
+// int[] randomArray = new int[lenArray];
+// for (int i = 0; i < randomArray.Length; i++)
+// {
+//     randomArray[i] = new Random().Next(1,9);
+//     Console.Write(randomArray[i] + " ");
+// }
+ Console.WriteLine("Введите любое число (A):");
+            enterNumber = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[enterNumber];
+
+            for (int i = 0; i < enterNumber; i++)
+            {
+                array[i] = new Random().Next(enterNumber+1);
+                Console.Write(array[i] + "; ");
+            }
+            Console.WriteLine();
